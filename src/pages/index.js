4d3a4e0c821987b64c77e7888de6c0d992027e70
Adam Avenir <adam@andyet.com>
2020-12-17 05:44:47 +0000

    polish

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,87 +6,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SiteIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
-
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="👋 Hi!" />
-        <Bio />
-        <p>No pages found. Add markdown posts to "content/pages".</p>
-      </Layout>
-    )
-  }
+  const siteTitle = "Sarah Avenir"
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="👋 Hi!" />
       <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                </header>
-                {post.frontmatter.description ? (
-                  <section>
-                    <p
-                      dangerouslySetInnerHTML={{
-                        __html: post.frontmatter.description,
-                      }}
-                      itemProp="description"
-                    />
-                  </section>
-                ) : (
-                  ""
-                )}
-              </article>
-            </li>
-          )
-        })}
-      </ol>
     </Layout>
   )
 }
 
 export default SiteIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(
-      filter: { fields: { collection: { eq: "pages" } } }
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      nodes {
-        excerpt
-        fields {
-          slug
-          collection
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
-      }
-    }
-  }
-`
