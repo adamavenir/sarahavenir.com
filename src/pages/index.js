eacb752f3b2db025c4cc4299dc68bac981bb6f1e
Sarah Avenir <sarah@andyet.net>
2023-08-08 14:46:31 -0700

    remove title?

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,4 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SiteIndex = ({ data, location }) => {
-  const siteTitle = "Sarah Avenir"
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <div className="global-wrapper">
-        <SEO title="👋 Hi!" />
-        <Bio />
-      </div>
-    </Layout>
-  )
-}
-
 export default SiteIndex
